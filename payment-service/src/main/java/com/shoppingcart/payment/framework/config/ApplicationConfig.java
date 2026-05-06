5f284052daf3b6dc7d0b8eb939b72e8b1ab1ca5d
ivan.marroquin <bryan.marroquin@coop1.com.sv>
2026-05-06 15:03:48 -0600

    fix: configure RestTemplate with HttpComponentsClientHttpRequestFactory and add httpclient5 dependency

--- payment-service/src/main/java/com/shoppingcart/payment/framework/config/ApplicationConfig.java
+++ payment-service/src/main/java/com/shoppingcart/payment/framework/config/ApplicationConfig.java
@@ -3,6 +3,7 @@ package com.shoppingcart.payment.framework.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import com.shoppingcart.payment.application.command.service.PaymentCommandService;
@@ -32,7 +33,7 @@ public class ApplicationConfig {
      */
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        return new RestTemplate(new HttpComponentsClientHttpRequestFactory());
     }
 
     /**
